a6dc969aff143d375a6fe400c9be13a78b275474
LqmKim <hakim19jan@gmail.com>
2023-09-25 10:06:39 +0800

    Changed title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -131,7 +131,7 @@ export default function IndexPage() {
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto max-w-2xl text-center">
               <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-                Data to enrich your online business
+                Fleet Tracking
               </h1>
               <p className="mt-6 text-lg leading-8 text-gray-600">
                 Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
