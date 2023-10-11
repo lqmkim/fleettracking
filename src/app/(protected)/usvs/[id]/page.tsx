c896b5834273bdc7a1c8607aa09c72ad74a6f6bf
LqmKim <hakim19jan@gmail.com>
2023-10-11 22:06:26 +0800

    debug

--- src/app/(protected)/usvs/[id]/page.tsx
+++ src/app/(protected)/usvs/[id]/page.tsx
@@ -1,31 +1,5 @@
 "use client";
 
-import useGoogleMapsLoader from "@/utils/client/useGoogleMapsLoader";
-import { useCallback, useState } from "react";
-
-const center = { lat: 3.23542, lng: 101.75081 };
-
 export default function FleetPage({ params }: { params: any }) {
-  const { isLoaded } = useGoogleMapsLoader();
-
-  const [map, setMap] = useState(null);
-
-  const onLoad = useCallback(function callback(map: any) {
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
-
-    setMap(map);
-  }, []);
-
-  const onUnmount = useCallback(function callback(map: any) {
-    setMap(null);
-  }, []);
-
-  return (
-    <div>
-      <div className="h-80 overflow-hidden rounded-lg bg-white shadow">
-        {/*  */}
-      </div>
-    </div>
-  );
+  return <div>{params.id}</div>;
 }
