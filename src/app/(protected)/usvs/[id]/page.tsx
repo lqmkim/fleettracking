03b9b31ed2f73472a2c4721ea2a920c9c331515d
LqmKim <hakim19jan@gmail.com>
2023-10-11 21:56:30 +0800

    debugging

--- src/app/(protected)/usvs/[id]/page.tsx
+++ src/app/(protected)/usvs/[id]/page.tsx
@@ -1,3 +1,40 @@
+import useGoogleMapsLoader from "@/utils/client/useGoogleMapsLoader";
+import { GoogleMap } from "@react-google-maps/api";
+import { useCallback, useState } from "react";
+
+const center = { lat: 3.23542, lng: 101.75081 };
+
 export default function FleetPage({ params }: { params: any }) {
-  return <h1>fleetpage {params.id}</h1>;
+  const { isLoaded } = useGoogleMapsLoader();
+
+  const [map, setMap] = useState(null);
+
+  const onLoad = useCallback(function callback(map: any) {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+
+    setMap(map);
+  }, []);
+
+  const onUnmount = useCallback(function callback(map: any) {
+    setMap(null);
+  }, []);
+
+  return (
+    <div>
+      <div className="h-80 overflow-hidden rounded-lg bg-white shadow">
+        {isLoaded && (
+          <GoogleMap
+            mapContainerStyle={{ width: "100%", height: "100%" }}
+            center={center}
+            zoom={10}
+            onLoad={onLoad}
+            onUnmount={onUnmount}
+          >
+            {/*  */}
+          </GoogleMap>
+        )}
+      </div>
+    </div>
+  );
 }
