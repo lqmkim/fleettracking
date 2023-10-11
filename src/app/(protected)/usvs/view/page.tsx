1cd1fe93736fd489e102bd24c48caf33d2da1c38
LqmKim <hakim19jan@gmail.com>
2023-10-11 22:33:38 +0800

    tests

--- src/app/(protected)/usvs/view/page.tsx
+++ src/app/(protected)/usvs/view/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import useUsv from "@/hooks/useUsv";
+import useUsvData from "@/hooks/useUsvData";
 import useGoogleMapsLoader from "@/utils/client/useGoogleMapsLoader";
 import { GoogleMap } from "@react-google-maps/api";
-import { useSearchParams } from "next/navigation";
+import { redirect, useSearchParams } from "next/navigation";
 import { useCallback, useState } from "react";
 
 const center = { lat: 3.23542, lng: 101.75081 };
@@ -10,6 +12,12 @@ const center = { lat: 3.23542, lng: 101.75081 };
 export default function FleetPage() {
   const searchParams = useSearchParams();
   const { isLoaded } = useGoogleMapsLoader();
+
+  const id = searchParams.get("id");
+  if (!id) redirect("/usvs");
+  const { usv } = useUsv(id);
+  const { usvData } = useUsvData(id);
+  console.log({ usv, usvData });
 
   const [map, setMap] = useState(null);
 
